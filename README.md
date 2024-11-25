d1577305320296fe74cb455f3875de1eaea8b299
Ordnarycoder <157827937+Ordnarycoder@users.noreply.github.com>
2024-11-25 09:53:13 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Implementation of graph to javascript.
---
+++ graph_ex.js
@@ -0,0 +1,61 @@
+class Graph {
+    constructor() {
+        //We will use adjacencylist to name vertex points.
+        this.adjacencyList = {};
+    }
+
+    // Add a vertex to the graph
+    addVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    // Add an edge between two vertices (Graph doesn't have a direction so we will do the same for both vertex.)
+    addEdge(vertex1, vertex2) {
+        if (!this.adjacencyList[vertex1]) {
+            this.addVertex(vertex1);
+        }
+        if (!this.adjacencyList[vertex2]) {
+            this.addVertex(vertex2);
+        }
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
+
+    // Remove an edge between two vertices
+    removeEdge(vertex1, vertex2) {
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    }
+
+    // Remove a vertex and all its edges
+    removeVertex(vertex) {
+        while (this.adjacencyList[vertex].length) {
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+    // Display the graph
+    show() {
+        for (let vertex in this.adjacencyList) {
+            console.log(vertex + " -> " + this.adjacencyList[vertex].join(", "));
+        }
+    }
+}
+
+// Example usage:
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "C");
+graph.show();
+// Output:
+// A -> B, C
+// B -> A, C
+// C -> A, B
